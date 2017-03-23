a9fa49edd102a5c7c1c9d3926ea5c2276b7ed1e8
munishgaurav5 <munishgaurav5@gmail.com>
2017-03-24 00:44:02 +0530

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -221,7 +221,8 @@ rm -rf /etc/letsencryp*
 yum -y install git bc
 git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
 #/opt/letsencrypt/letsencrypt-auto certonly --standalone --agree-tos --email myemail@$WEBMAIL_DOMAIN -d $WEBMAIL_DOMAIN -d www.$WEBMAIL_DOMAIN -d $VH_ROUNCUBE -d $VH_POSTFIXADMIN -d $SRV_ALIAS -d
-/opt/letsencrypt/letsencrypt-auto certonly --standalone --agree-tos --email myemail@$WEBMAIL_DOMAIN -d $HOSTNAME_WEB -d $VH_ROUNCUBE -d $VH_POSTFIXADMIN -d $SRV_ALIAS -d
+/opt/letsencrypt/letsencrypt-auto certonly --standalone --agree-tos --email myemail@$WEBMAIL_DOMAIN -d $HOSTNAME_WEB -d $VH_ROUNCUBE -d $VH_POSTFIXADMIN -d $SRV_ALIAS
+#/opt/letsencrypt/letsencrypt-auto renew
 
 chown -R root:root /etc/letsencrypt/live/
 
