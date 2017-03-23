5d0bdb68c919fea10be822cae5ea8f6dfdea204e
munishgaurav5 <munishgaurav5@gmail.com>
2017-03-24 01:02:07 +0530

    Update ms.sh

--- ms.sh
+++ ms.sh
@@ -111,6 +111,29 @@ WEB_LATEST_POSTFIXADM='https://raw.github.com/munishgaurav5/st/master/pfa302.tar
 #WEB_LATEST_POSTFIXADM='https://raw.github.com/munishgaurav5/st/master/pfa302.tar.gz'
 
 ## </Variabile de schimbat> ###
+############### REQ SOFTWARES INSTALL ##################
+
+hostnamectl set-hostname $HOSTNAME_WEB
+hostname
+
+echo "$SRV_MAIL_IP $HOSTNAME_WEB $WEBMAIL_HOST" >> /etc/hosts
+
+
+cd /tmp
+
+
+yum -y update
+yum -y install nano wget curl net-tools lsof bzip2 zip unzip rar unrar epel-release git sudo make cmake GeoIP sed at
+
+yum -y update
+
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install gcc gcc-c++ pcre pcre-devel zlib zlib-devel mailx expect imake lsof autoconf nc ca-certificates libedit-devel make automake expat-devel perl-libwww-perl perl-Crypt-SSLeay perl-Net-SSLeay tree virt-what cmake openssl-devel net-tools systemd-devel libdb-devel libxslt-devel gd gd-devel perl-ExtUtils-Embed patch sysstat libtool bind-utils libXext-devel cyrus-sasl-devel glib2 glib2-devel openssl ncurses-devel bzip2 bzip2-devel flex bison libcurl-devel which libevent libevent-devel libgcj gettext-devel vim-minimal nano cairo-devel libxml2-devel libxml2 libpng-devel freetype freetype-devel libart_lgpl-devel  GeoIP-devel gperftools-devel libicu libicu-devel aspell gmp-devel aspell-devel libtidy libtidy-devel readline-devel iptables* coreutils libedit-devel enchant enchant-devel pam-devel git perl-ExtUtils perl-ExtUtils-MakeMaker perl-Time-HiRes openldap openldap-devel curl curl-devel diffutils libc-client libc-client-devel numactl lsof pkgconfig gdbm-devel tk-devel bluez-libs-devel
+sudo yum -y install unzip zip rar unrar rsync psmisc syslog-ng-libdbi mediainfo
+
+
+########################################################
+
 
 ### install ##################################################
 # add mail user
@@ -220,7 +243,7 @@ rm -rf /etc/letsencryp*
 
 yum -y install git bc
 git clone https://github.com/letsencrypt/letsencrypt /opt/letsencrypt
-#/opt/letsencrypt/letsencrypt-auto certonly --standalone --agree-tos --email myemail@$WEBMAIL_DOMAIN -d $WEBMAIL_DOMAIN -d www.$WEBMAIL_DOMAIN -d $VH_ROUNCUBE -d $VH_POSTFIXADMIN -d $SRV_ALIAS -d
+#/opt/letsencrypt/letsencrypt-auto certonly --standalone --agree-tos --email myemail@$WEBMAIL_DOMAIN -d $WEBMAIL_DOMAIN -d www.$WEBMAIL_DOMAIN -d $VH_ROUNCUBE -d $VH_POSTFIXADMIN -d $SRV_ALIAS
 /opt/letsencrypt/letsencrypt-auto certonly --standalone --agree-tos --email myemail@$WEBMAIL_DOMAIN -d $HOSTNAME_WEB -d $VH_ROUNCUBE -d $VH_POSTFIXADMIN -d $SRV_ALIAS
 #/opt/letsencrypt/letsencrypt-auto renew
 
