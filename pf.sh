579ec587fdf54c0a954257d4c01400d0c3f0edc4
munishgaurav5 <munishgaurav5@gmail.com>
2017-03-26 18:46:35 +0530

    Update pf.sh

--- pf.sh
+++ pf.sh
@@ -1,106 +1,22 @@
 #!/bin/bash
 
-#------------------------------------------------------------------------------------
-# Params
-#------------------------------------------------------------------------------------
-
-FTP_USER=$1
-if [ -z $FTP_USER ] 
-then
-	echo -e "What is the default FTP user: \c "
-	read FTP_USER
-fi
-
-SERVER_NAME=$2
-if [ -z $SERVER_NAME ] 
-then
-	echo -e "What is the full server name: \c "
-	read SERVER_NAME
-fi
-
-#------------------------------------------------------------------------------------
-# Install vsFTPd
-#------------------------------------------------------------------------------------
-
-yum -y install vsftpd
-
-#------------------------------------------------------------------------------------
-# Configure
-#------------------------------------------------------------------------------------
-
-useradd -s /sbin/nologin -d /var/www/html vsftpd
-chown -R vsftpd:vsftpd /var/www/html
-
-mkdir -p /etc/vsftpd/vconf
-cp /etc/vsftpd/vsftpd.conf{,.original}
- 
-sed -i "s/^.*anonymous_enable.*/anonymous_enable=NO/g" /etc/vsftpd/vsftpd.conf
-sed -i "/^xferlog_std_format*a*/ s/^/#/" /etc/vsftpd/vsftpd.conf
-sed -i "s/#idle_session_timeout=600/idle_session_timeout=900/" /etc/vsftpd/vsftpd.conf
-sed -i "s/#nopriv_user=ftpsecure/nopriv_user=vsftpd/" /etc/vsftpd/vsftpd.conf
-sed -i "/#chroot_list_enable=YES/i\chroot_local_user=YES" /etc/vsftpd/vsftpd.conf
-sed -i 's/listen=NO/listen=YES/' /etc/vsftpd/vsftpd.conf
-sed -i 's/listen_ipv6=YES/listen_ipv6=NO/' /etc/vsftpd/vsftpd.conf
- 
-echo 'allow_writeable_chroot=YES
-guest_enable=YES
-guest_username=vsftpd
-local_root=/var/www/html
-user_sub_token=$USER
-virtual_use_local_privs=YES
-user_config_dir=/etc/vsftpd/vconf' >> /etc/vsftpd/vsftpd.conf
+POSTFIX_HASH=''
+#DKIM_SELECTOR=$2
 
 
-echo "
-rsa_cert_file=/etc/letsencrypt/live/$SERVER_NAME/cert.pem
-rsa_private_key_file=/etc/letsencrypt/live/$SERVER_NAME/privkey.pem
+   while [[ $POSTFIX_HASH = "" ]]; do # to be replaced with regex
+        read -p "ENTER POSTFIX ADMIN HASH: " POSTFIX_HASH
+    done
 
-ssl_enable=YES
-allow_anon_ssl=NO
-force_local_data_ssl=YES
-force_local_logins_ssl=YES
+	
+    echo "Working..."
+sleep 3
 
-ssl_tlsv1=YES
-ssl_sslv2=NO
-ssl_sslv3=NO
+echo "\$CONF['setup_password'] = '$POSTFIX_HASH';" >> /var/www/html/postfixadmin/config.inc.php
+echo 'Location : /var/www/html/postfixadmin/config.inc.php'
+echo '...'
 
-require_ssl_reuse=NO
-ssl_ciphers=HIGH" >> /etc/vsftpd/vsftpd.conf
+echo 'PostFixAdmin Configured!'
 
+echo '#################  DONE  ######################'
 
-
-
-systemctl start vsftpd.service
-systemctl enable vsftpd.service
-
-#------------------------------------------------------------------------------------
-# Configure pam
-#------------------------------------------------------------------------------------
- 
-cp /etc/pam.d/vsftpd{,.original}
- 
-echo '#%PAM-1.0
-auth required pam_userdb.so db=/etc/vsftpd/password crypt=crypt
-account required pam_userdb.so db=/etc/vsftpd/password crypt=crypt
-session required pam_loginuid.so' > /etc/pam.d/vsftpd
-
-#------------------------------------------------------------------------------------
-# Create default user
-#------------------------------------------------------------------------------------
-
-# /etc/vsftpd/vconf/defaultuser
-echo 'dirlist_enable=YES
-download_enable=YES
-local_root=/var/www/html
-write_enable=YES' > /etc/vsftpd/vconf/$FTP_USER
- 
-echo "$FTP_USER" | tee /etc/vsftpd/password{,-nocrypt} > /dev/null
- 
-FTP_PASS=$(openssl rand -base64 6)
-echo $FTP_PASS >> /etc/vsftpd/password-nocrypt
-echo $(openssl passwd -crypt $FTP_PASS) >> /etc/vsftpd/password
- 
-# /etc/vsftpd/password.db
-db_load -T -t hash -f /etc/vsftpd/password /etc/vsftpd/password.db
-
-FTP_DEFAULT_PASS=$FTP_PASS
