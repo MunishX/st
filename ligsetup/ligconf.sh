4b825c584eaf4629b56828d9b7111c221ade0611
munishgaurav5 <munishgaurav5@gmail.com>
2017-04-12 03:31:12 +0530

    Update ligconf.sh

--- ligsetup/ligconf.sh
+++ ligsetup/ligconf.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-#### LIGHTTPD CONFIG
+mv  /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.old
+mv /etc/lighttpd/conf.d/fastcgi.conf /etc/lighttpd/conf.d/fastcgi.old
+mv /etc/lighttpd/modules.conf /etc/lighttpd/modules.old
 
+#### LIGHTTPD CONFIG
+wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/ligcnf -O /etc/lighttpd/lighttpd.conf
 
 #### FASTCGI CONFIG
-
+wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/fgicnf -O /etc/lighttpd/conf.d/fastcgi.conf
 
 #### MODULES CONFIG
+wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/modcnf -O /etc/lighttpd/modules.conf
+
 
 
 #### PHP CONFIG
