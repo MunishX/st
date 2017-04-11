b102a06fb383d00cec107a881201415457ca28b0
munishgaurav5 <munishgaurav5@gmail.com>
2017-04-12 02:14:48 +0530

    Update ligconf.sh

--- ligsetup/ligconf.sh
+++ ligsetup/ligconf.sh
@@ -1,75 +1,61 @@
 #!/bin/bash
 
-chown -R admin:admin /var/opt/remi/php70
-
-#### PHP WWW Admin Config
-
-sed -i "s/^.*user = apache.*/user = admin/" /etc/opt/remi/php70/php-fpm.d/www.conf
-sed -i "s/^.*group = apache.*/group = admin/" /etc/opt/remi/php70/php-fpm.d/www.conf
-sed -i "s,^.*listen = 127.0.0.1:9000.*,listen = /run/php-admin.sock," /etc/opt/remi/php70/php-fpm.d/www.conf
-sed -i "s/^.*listen.mode = 06.*/listen.mode = 0666/" /etc/opt/remi/php70/php-fpm.d/www.conf
-sed -i "s/^.*listen.acl_users = .*/listen.acl_users = admin/" /etc/opt/remi/php70/php-fpm.d/www.conf
-
-sed -i "s/^.*Start a new pool named 'www'.*/[global]/" /etc/opt/remi/php70/php-fpm.d/www.conf
-sed -i "s,^.*expose_php =.*,pid = /run/php-admin.pid," /etc/opt/remi/php70/php-fpm.d/www.conf
-sed -i "s,^.*pm.status_path =.*,pm.status_path = /status/php.php ," /etc/opt/remi/php70/php-fpm.d/www.conf
-
-#####
-
-#### Lighttpd Config File
-echo '
-server.stream-response-body = 1
-server.tag = "LiteSpeed Web Server"
-include_shell "cat /etc/lighttpd/enabled/*.conf"
-' >> /etc/lighttpd/lighttpd.conf
-
-sed -i 's/^.*server.use-ipv6 =.*/server.use-ipv6 = "disable"/' /etc/lighttpd/lighttpd.conf
-sed -i 's,^.*server.document-root =.*,server.document-root = "/home/admin/web/ip",' /etc/lighttpd/lighttpd.conf
-sed -i 's/^.*server.network-backend =.*/server.network-backend = "writev"/' /etc/lighttpd/lighttpd.conf
-sed -i 's/^.*server.max-fds =.*/server.max-fds = 6048/' /etc/lighttpd/lighttpd.conf
-sed -i 's/^.*server.max-connections =.*/server.max-connections = 3000/' /etc/lighttpd/lighttpd.conf
-#sed -i 's/^.*__.*/__/' /etc/lighttpd/lighttpd.conf
-#sed -i 's/^.*__.*/__/' /etc/lighttpd/lighttpd.conf
-#sed -i 's/^.*__.*/__/' /etc/lighttpd/lighttpd.conf
-
-####
-
-#### Modules Config
-/etc/lighttpd/modules.conf
-sed -i 's/^.*"mod_alias",.*/  "mod_alias",/' /etc/lighttpd/modules.conf
-sed -i 's/^.*"mod_auth",.*/  "mod_auth",/' /etc/lighttpd/modules.conf
-sed -i 's/^.*"mod_authn_file",.*/  "mod_authn_file",/' /etc/lighttpd/modules.conf
-sed -i 's/^.*"mod_evasive",.*/  "mod_evasive",/' /etc/lighttpd/modules.conf
-sed -i 's/^.*"mod_redirect",.*/  "mod_redirect",/' /etc/lighttpd/modules.conf
-sed -i 's/^.*"mod_rewrite",.*/  "mod_rewrite",/' /etc/lighttpd/modules.conf
-sed -i 's/^.*"mod_setenv",.*/  "mod_setenv",/' /etc/lighttpd/modules.conf
-sed -i 's/^.*"mod_usertrack",.*/  "mod_usertrack",/' /etc/lighttpd/modules.conf
-
-sed -i 's/^.*include "conf.d/expire.conf".*/  include "conf.d/expire.conf"/' /etc/lighttpd/modules.conf
-sed -i 's/^.*include "conf.d/fastcgi.conf".*/  include "conf.d/fastcgi.conf"/' /etc/lighttpd/modules.conf
-sed -i 's/^.*include "conf.d/cgi.conf".*/  include "conf.d/cgi.conf"/' /etc/lighttpd/modules.conf
-
-####
-
-#### Fastcgi Config
-
-echo '
-server.modules += ( "mod_fastcgi" )
-# FastCGI unix socket setting for ADMIN
-fastcgi.server += ( ".php" =>
-        ((
-#                "host" => "127.0.0.1",
-#                "port" => "9000",
-                "socket" => "/run/php-admin.sock",
-                "broken-scriptfilename" => "enable"
-        ))
-)
-' > /etc/lighttpd/conf.d/fastcgi.conf
-
-#####
+#### LIGHTTPD CONFIG
 
 
-## enabled folder
-## sock php
-## /home/admin/web/ip
-## lighttpd user lighttpd
+#### FASTCGI CONFIG
+
+
+#### MODULES CONFIG
+
+
+#### PHP CONFIG
+
+uname=$1
+admin_username=$2
+
+php_add_head=php
+software_name=${php_add_head}-${uname}
+user_root=/home/$uname/
+user_php=php
+admin_bin_loc=/home/$admin_username/bin/
+
+wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/www -O $user_root$user_php/$software_name.conf
+sed -i "s,^.*/run/php-fpm-pool.pid.*,pid = $user_root$user_php/$software_name.pid," $user_root$user_php/$software_name.conf
+sed -i "s/^.*www-name.*/[$software_name]/" $user_root$user_php/$software_name.conf
+sed -i "s/^.*user-name.*/user = $uname/" $user_root$user_php/$software_name.conf
+sed -i "s/^.*group-name.*/group = $uname/" $user_root$user_php/$software_name.conf
+sed -i "s,^.*/run/php70-php-fpm.sock.*,listen = $user_root$user_php/$software_name.sock," $user_root$user_php/$software_name.conf
+sed -i "s/^.*listen-u-name.*/listen.acl_users = $uname/" $user_root$user_php/$software_name.conf
+sed -i "s,/user-php-root/,$user_root$user_php/,g" $user_root$user_php/$software_name.conf
+
+wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/intl -O $admin_bin_loc/$software_name
+#sed -i "s/^.*php-fpm-bin.*/php_fpm_BIN=php-$uname/" $startup_root$uname
+sed -i "s,^.*/etc/opt/remi/php70/php-fpm.d/www.conf.*,php_fpm_CONF=$user_root$user_php/$software_name.conf," $admin_bin_loc/$software_name
+sed -i "s,^.*/etc/opt/remi/php70/php-fpm.d/php-fpm.pid.*,php_fpm_PID=$user_root$user_php/$software_name.pid," $admin_bin_loc/$software_name
+chmod 777 $admin_bin_loc/$software_name
+
+chown -R $uname:$uname $user_root
+chown -R $admin_username:$uname $user_root/logs
+
+echo "Done!!!!!"
+
+bash $admin_bin_loc/$software_name start
+
+#chown -R admin:admin /var/opt/remi/php70
+#####################
+
+#fastcgi.conf
+#Admin php sock
+
+
+#lighttpd.conf
+## enabled folder : /etc/lighttpd/enabled/*.conf
+## admin php sock : /run/php-admin.sock
+## ip home : /home/admin/html/ip
+## user : admin:admin
+## folders : html,logs,php,bin
+#modules.conf
+#ok
+
+
