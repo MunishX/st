b618cfc7101a8724659ea08a9da3529c025aec61
munishgaurav5 <munishgaurav5@gmail.com>
2017-04-12 03:14:01 +0530

    Update ligconf.sh

--- ligsetup/ligconf.sh
+++ ligsetup/ligconf.sh
@@ -11,14 +11,169 @@
 
 #### PHP CONFIG
 
+############################### ADDED START
+
+# Take input for username and password
 uname=$1
-admin_username=$2
+   while [[ $uname = "" ]]; do # to be replaced with regex
+       read -p "VHost username: " uname
+    done
+
+passw=$2
+   while [[ $passw = "" ]]; do # to be replaced with regex
+       read -p "$uname's Password: " passw
+    done
+
+mydom=$3
+   while [[ $mydom = "" ]]; do # to be replaced with regex
+       read -p "$uname's Domain: " mydom
+    done
+
+admin_username=$3
+   while [[ $admin_username = "" ]]; do # to be replaced with regex
+       read -p "ADMIN USERNAME: " admin_username
+    done
+
+#read -p "Transmission username: " uname
+#read -p "$uname's Password: " passw
+
+
+
+#uname=$1
+#admin_username=$4
 
 php_add_head=php
 software_name=${php_add_head}-${uname}
 user_root=/home/$uname/
 user_php=php
 admin_bin_loc=/home/$admin_username/bin/
+
+
+#### Check Status
+USERID=${uname}
+STOP_IT=0
+
+/bin/id -u $USERID 2>/dev/null
+if [ $? -eq 0 ]; then
+   echo "User $USERID already exists."   
+else
+   echo "UserID : OK"
+fi
+
+/bin/id -g $USERID 2>/dev/null
+if [ $? -eq 0 ]; then
+   echo "Group $USERID already exists."
+else
+   echo "GroupID : OK"
+fi
+
+
+domain_loc=${user_root}php/$software_name.conf
+if [ -f "$domain_loc" ]
+then
+	echo "Domain : Error "
+        STOP_IT=1
+else
+	echo "Domain : OK "
+   install_type="y"
+fi
+
+
+if [[ $STOP_IT = 1 ]]; then
+echo 'Error! Domain already available. Please add new domain and try again.'
+exit 1
+fi
+
+echo "Starting Install : All OK! "
+sleep 3
+
+###########
+
+
+
+
+###########################################
+
+###### EXTRA ADDING 
+
+#####################################################
+#####################################################
+
+
+
+############################################################
+########################## START INSTALL ##################################
+
+# Update system and install required packages
+yum -y update
+yum -y install gcc gcc-c++ m4 xz make automake curl-devel intltool libtool gettext openssl-devel perl-Time-HiRes wget
+
+yum -y update
+yum -y install nano wget curl net-tools lsof bzip2 zip unzip rar unrar epel-release git sudo make cmake GeoIP sed at
+
+yum -y update
+
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install gcc gcc-c++ pcre pcre-devel zlib zlib-devel mailx expect imake lsof autoconf nc ca-certificates libedit-devel make automake expat-devel perl-libwww-perl perl-Crypt-SSLeay perl-Net-SSLeay tree virt-what cmake openssl-devel net-tools systemd-devel libdb-devel libxslt-devel gd gd-devel perl-ExtUtils-Embed patch sysstat libtool bind-utils libXext-devel cyrus-sasl-devel glib2 glib2-devel openssl ncurses-devel bzip2 bzip2-devel flex bison libcurl-devel which libevent libevent-devel libgcj gettext-devel vim-minimal nano cairo-devel libxml2-devel libxml2 libpng-devel freetype freetype-devel libart_lgpl-devel  GeoIP-devel gperftools-devel libicu libicu-devel aspell gmp-devel aspell-devel libtidy libtidy-devel readline-devel iptables* coreutils libedit-devel enchant enchant-devel pam-devel git perl-ExtUtils perl-ExtUtils-MakeMaker perl-Time-HiRes openldap openldap-devel curl curl-devel diffutils libc-client libc-client-devel numactl lsof pkgconfig gdbm-devel tk-devel bluez-libs-devel
+sudo yum -y install unzip zip rar unrar rsync psmisc syslog-ng-libdbi mediainfo
+yum -y install gcc gcc-c++ m4 xz make automake curl-devel intltool libtool gettext openssl-devel perl-Time-HiRes wget
+
+
+#Create UNIX user and directories for transmission
+encrypt_pass=$(perl -e 'print crypt($ARGV[0], "password")' $passw)
+useradd -m -p $encrypt_pass $uname
+
+sed -i "s/^\($uname.*\)$/\1$uname,$admin_username/g" /etc/group
+
+
+mkdir -p $user_root{html,php,logs}
+mkdir -p $user_rootphp/{session,wsdlcache,opcache,log}
+touch $user_roothtml/status/php.php
+
+chmod g+w $user_root
+chmod -R 777 $user_rootphp
+
+chown -R $uname:$uname $user_root
+chown -R $admin_username:$uname $user_rootlogs
+
+
+ echo "
+ 
+ \$HTTP[\"host\"] == \"$mydom\" {
+    server.document-root = \"$user_roothtml\" 
+    accesslog.filename = \"$user_rootlogs/access_log.txt\" 
+   # fastcgi.map-extensions = (".fpm" => ".php")
+    fastcgi.server = ( \".php\" =>
+                       (
+                          (
+			    \"socket\" => \"$user_rootphp/$software_name.sock\",
+                            \"broken-scriptfilename\" => \"enable\" 
+                          )
+                        )
+                      )
+}
+  " > /etc/lighttpd/enabled/$mydom.conf
+
+ 
+ echo '
+<?php
+echo "<h1>Hello World!</h1>";
+echo "<p>Current User ID is: ". posix_getuid();
+echo "<p>Current Group ID is: ". posix_getgid();
+?>
+' > $user_roothtml/test.php
+
+chown -R $uname:$uname $user_roothtml
+
+if [ $admin_username != $uname ]; then
+systemctl restart  lighttpd.service
+fi
+
+
+echo "Done!"
+
+
+###################
 
 wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/www -O $user_root$user_php/$software_name.conf
 sed -i "s,^.*/run/php-fpm-pool.pid.*,pid = $user_root$user_php/$software_name.pid," $user_root$user_php/$software_name.conf
