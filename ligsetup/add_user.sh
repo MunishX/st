4b6a7dca9d7d933ae9212ca53892b6dce3f3f5c8
munishgaurav5 <munishgaurav5@gmail.com>
2017-04-15 12:47:03 +0530

    Update add_user.sh

--- ligsetup/add_user.sh
+++ ligsetup/add_user.sh
@@ -38,7 +38,7 @@ fi
 Restart_Lig=$6
 echo ""
    while [[ $Restart_Lig = "" ]]; do # to be replaced with regex
-       read -p "(6/6) INSTALL Torrent (y/n): " Restart_Lig
+       read -p "(6/6) Restart Lighttpd (y/n): " Restart_Lig
     done
 
 # Admin_User=$6
