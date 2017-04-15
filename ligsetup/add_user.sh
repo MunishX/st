25c52e97020e6c9332fbdb2fbc5dffe70dae4917
munishgaurav5 <munishgaurav5@gmail.com>
2017-04-15 12:34:32 +0530

    Update add_user.sh

--- ligsetup/add_user.sh
+++ ligsetup/add_user.sh
@@ -5,24 +5,24 @@
 User_Name=$1
 echo ""
    while [[ $User_Name = "" ]]; do # to be replaced with regex
-       read -p "(1/9) Enter Username (user): " User_Name
+       read -p "(1/6) Enter Username (user): " User_Name
     done
     
 User_Pass=$2
 echo ""
    while [[ $User_Pass = "" ]]; do # to be replaced with regex
-       read -p "(2/9) $User_Name's Password (pass): " User_Pass
+       read -p "(2/6) $User_Name's Password (pass): " User_Pass
     done
 
 mydom=$3
    while [[ $mydom = "" ]]; do # to be replaced with regex
-       read -p "(3/9) $User_Name's Domain: " mydom
+       read -p "(3/6) $User_Name's Domain: " mydom
     done
 
 Install_Torrent=$4
 echo ""
    while [[ $Install_Torrent = "" ]]; do # to be replaced with regex
-       read -p "(4/9) INSTALL Torrent (y/n): " Install_Torrent
+       read -p "(4/6) INSTALL Torrent (y/n): " Install_Torrent
     done
 
 if [ $Install_Torrent = "y" ]; then
@@ -30,10 +30,16 @@ if [ $Install_Torrent = "y" ]; then
    Torrent_Port=$5
    echo ""
    while [[ $Torrent_Port = "" ]]; do # to be replaced with regex
-       read -p "(4/9) Torrent Port (9091): " Torrent_Port
+       read -p "(5/6) Torrent Port (9091): " Torrent_Port
     done
   
 fi
+
+Restart_Lig=$6
+echo ""
+   while [[ $Restart_Lig = "" ]]; do # to be replaced with regex
+       read -p "(6/6) INSTALL Torrent (y/n): " Restart_Lig
+    done
 
 # Admin_User=$6
 # echo ""
@@ -52,11 +58,11 @@ cd /tmp/add_user_script
 #------------------------------------------------------------------------------------
 # Install LIG CONFIG 
 #------------------------------------------------------------------------------------
-restart_no=y
+#restart_no=y
 
 wget https://github.com/munishgaurav5/st/raw/master/ligsetup/create_vhost.sh
 chmod 777 create_vhost.sh
-./create_vhost.sh $User_Name $User_Pass $mydom $Admin_User $restart_no
+./create_vhost.sh $User_Name $User_Pass $mydom $Admin_User $Restart_Lig
 
 echo ""
 echo ""
