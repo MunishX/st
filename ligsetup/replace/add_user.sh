f6ef4f93709b34c5d6563c90ddc6967e86b220dc
munishgaurav5 <munishgaurav5@gmail.com>
2017-04-23 18:00:52 +0530

    Update add_user.sh

--- ligsetup/replace/add_user.sh
+++ ligsetup/replace/add_user.sh
@@ -60,9 +60,9 @@ cd /tmp/add_user_script
 #------------------------------------------------------------------------------------
 #restart_no=y
 
-wget https://github.com/munishgaurav5/st/raw/master/ligsetup/create_vhost.sh
-chmod 777 create_vhost.sh
-./create_vhost.sh $User_Name $User_Pass $mydom $Admin_User $Restart_Lig
+wget https://github.com/munishgaurav5/st/raw/master/ligsetup/10_create_vhost.sh
+chmod 777 10_create_vhost.sh
+./10_create_vhost.sh $User_Name $User_Pass $mydom $Admin_User $Restart_Lig
 
 echo ""
 echo ""
@@ -84,9 +84,9 @@ sleep 10
 if [[ $Install_Torrent = "y" ]]; then
 
 old_user_true=y
-wget https://github.com/munishgaurav5/st/raw/master/ligsetup/tmm.sh
-chmod 777 tmm.sh 
-./tmm.sh $User_Name $User_Pass $Torrent_Port $mydom $Admin_User $old_user_true
+wget https://github.com/munishgaurav5/st/raw/master/ligsetup/12_tmm.sh
+chmod 777 12_tmm.sh 
+./12_tmm.sh $User_Name $User_Pass $Torrent_Port $mydom $Admin_User $old_user_true
 
 echo ""
 echo ""
