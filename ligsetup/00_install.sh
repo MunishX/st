4f785c1b79b8e146f1a44f1273d83241014981d6
munishgaurav5 <munishgaurav5@gmail.com>
2017-04-23 18:25:55 +0530

    Update 00_install.sh

--- ligsetup/00_install.sh
+++ ligsetup/00_install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+umask
+umask 0002
+umask
+
+sleep 6
 # yum -y install wget && cd /tmp && wget https://github.com/munishgaurav5/st/raw/master/ligsetup/install.sh && chmod 777 install.sh && ./install.sh
 # yum -y install wget nano && cd /tmp && wget https://github.com/munishgaurav5/st/raw/master/ligsetup/install.sh && chmod 777 install.sh && nano install.sh 
 
