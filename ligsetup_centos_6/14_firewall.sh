9cbb69232dfe0e94a8ae4bdc4864a994fc957178
munishgaurav5 <munishgaurav5@gmail.com>
2017-04-26 00:02:47 +0530

    Update 14_firewall.sh

--- ligsetup_centos_6/14_firewall.sh
+++ ligsetup_centos_6/14_firewall.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 
-firewall-cmd --permanent --zone=public --add-service=http
-firewall-cmd --permanent --zone=public --add-service=https
-firewall-cmd --permanent --zone=public --add-port=789/tcp  # Prutunel VPN
-firewall-cmd --reload
+#firewall-cmd --permanent --zone=public --add-service=http
+#firewall-cmd --permanent --zone=public --add-service=https
+#firewall-cmd --permanent --zone=public --add-port=789/tcp  # Prutunel VPN
+#firewall-cmd --reload
 
 
 
-systemctl mask firewalld
-systemctl stop firewalld
-systemctl disable firewalld
+#systemctl mask firewalld
+#systemctl stop firewalld
+#systemctl disable firewalld
 
+service iptables stop
+service firewalld stop
+
+chkconfig iptables off
+chkconfig firewalld off
