32151fd777b23f6ed86917dc55fb21055c15bba5
munishgaurav5 <munishgaurav5@gmail.com>
2017-05-07 20:32:03 +0530

    Update ff_in.sh

--- ff_in.sh
+++ ff_in.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+
+
+
+###### VARS #####
+PreFix_Dir=/usr
+
+#################
+
+
+
 # A Linux Shell Script for compiling and installing ffmpeg in CentOS (tested on 6).
 # This script based on ffmpeg guide <http://ffmpeg.org/trac/ffmpeg/wiki/CentosCompilationGuide>
 # This script is licensed under GNU GPL version 3.0 or above.
@@ -12,106 +22,237 @@ sudo yum -y erase ffmpeg x264 x264-devel
 sudo yum -y install autoconf automake gcc gcc-c++ git libtool make nasm pkgconfig zlib-devel
 
 
-mkdir ~/ffmpeg_sources
+
+
+mkdir -p $PreFix_Dir/{src,bin,lib,include}
+
+
+
+cd /tmp
+yum -y update
+yum -y install nano wget curl net-tools lsof bzip2 zip unzip rar unrar git sed epel-release
+yum -y install glibc gcc gcc-c++ autoconf automake libtool git make nasm pkgconfig SDL-devel a52dec a52dec-devel 
+yum -y install alsa-lib-devel faac faac-devel faad2 faad2-devel freetype-devel giflib gsm gsm-devel 
+yum -y install imlib2 imlib2-devel lame lame-devel libICE-devel libSM-devel libX11-devel libXau-devel 
+yum -y install libXdmcp-devel libXext-devel libXrandr-devel libXrender-devel libXt-devel libogg libvorbis
+yum -y install vorbis-tools mesa-libGL-devel mesa-libGLU-devel xorg-x11-proto-devel zlib-devel libtheora
+yum -y install theora-tools ncurses-devel libdc1394 libdc1394-devel amrnb-devel amrwb-devel opencore-amr-devel
+yum -y install autoconf automake cmake freetype-devel gcc gcc-c++ git libtool make mercurial nasm pkgconfig zlib-devel
+yum -y install libass-devel libass
+
+
+###########################################################
+# yasm 1.3.0
+# original: http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+###########################################################
 
 # install Yasm
-cd ~/ffmpeg_sources
+cd $PreFix_Dir/src
 curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
 tar xzvf yasm-1.2.0.tar.gz
 cd yasm-1.2.0
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+./configure --prefix="$PreFix_Dir" --bindir="$PreFix_Dir/bin"
 make
 make install
 make distclean
 . ~/.bash_profile
 
-# install x264
-cd ~/ffmpeg_sources
-git clone --depth 1 git://git.videolan.org/x264
-cd x264
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
-make
-make install
-make distclean
 
-# install libfdk_aac
-cd ~/ffmpeg_sources
-git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
-cd fdk-aac
-autoreconf -fiv
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
-make
-make install
-make distclean
+###########################################################
+# lame 3.99.5
+# original: http://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz/download
+###########################################################
 
 # install libmp3lame
-cd ~/ffmpeg_sources
+cd $PreFix_Dir/src
 curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
 tar xzvf lame-3.99.5.tar.gz
 cd lame-3.99.5
-./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --disable-shared --enable-nasm
+./configure --prefix="$PreFix_Dir" --bindir="$PreFix_Dir/bin"  --disable-shared --enable-nasm
 make
 make install
 make distclean
 
-# install libopus
-cd ~/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/opus/opus-1.0.3.tar.gz
-tar xzvf opus-1.0.3.tar.gz
-cd opus-1.0.3
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
-make
-make install
-make distclean
 
+###########################################################
+# libogg 1.3.2
+# original: http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+###########################################################
 # install libogg
-cd ~/ffmpeg_sources
+cd $PreFix_Dir/src
 curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.1.tar.gz
 tar xzvf libogg-1.3.1.tar.gz
 cd libogg-1.3.1
-./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+./configure --prefix="$PreFix_Dir" --disable-shared
 make
 make install
 make distclean
 
+
+###########################################################
+# libvorbis 1.3.5
+# original: http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
+###########################################################
+
 # install libvorbis
-cd ~/ffmpeg_sources
+cd $PreFix_Dir/src
 curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.3.tar.gz
 tar xzvf libvorbis-1.3.3.tar.gz
 cd libvorbis-1.3.3
-./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --disable-shared
+./configure --prefix="$PreFix_Dir" --with-ogg="$PreFix_Dir" --disable-shared
 make
 make install
 make distclean
 
+
+###########################################################
+# libtheora 1.1.1
+# original: http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.gz
+###########################################################
+
+
 # install libtheora
-cd ~/src/ffmpeg-source
+cd $PreFix_Dir/src
 wget http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.gz
 tar xzvf libtheora-1.1.1.tar.gz
 cd libtheora-1.1.1
-./configure --disable-shared
-make
-sudo make install
+./configure --prefix="$PreFix_Dir" --disable-shared
+make 
+make install
 
+###########################################################
+# libvpx @ d6996849f0c65b97c40318647c3e7dc2db332861
+# original: https://chromium.googlesource.com/webm/libvpx
+###########################################################
 
 # install libvpx
-cd ~/ffmpeg_sources
+cd $PreFix_Dir/src
 git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
 cd libvpx
-./configure --prefix="$HOME/ffmpeg_build" --disable-examples
+./configure  --prefix="$PreFix_Dir" --disable-examples
 make
 make install
 make clean
 
+###########################################################
+# libx264 @ a01e33913655f983df7a4d64b0a4178abb1eb618
+# original: git://git.videolan.org/x264.git
+###########################################################
+
+# install x264
+cd $PreFix_Dir/src
+git clone --depth 1 git://git.videolan.org/x264
+cd x264
+./configure --prefix="$PreFix_Dir" --bindir="$PreFix_Dir/bin" --enable-static
+make
+make install
+make distclean
+
+
+###########################################################
+# libxvid 1.3.4
+# original: http://downloads.xvid.org/downloads/xvidcore-1.3.4.tar.gz
+###########################################################
+cd $PreFix_Dir/src
+wget 'http://mirror.ryansanden.com/ffmpeg-9079e99d/xvidcore-1.3.4.tar.gz'
+tar -xf xvidcore-1.3.4.tar.gz
+cd xvidcore/build/generic
+./configure --prefix="$PreFix_Dir"
+make
+make install
+
+
+
+###########################################################
+# EXTRA @ xyz 1 // -> opus
+# original: git://source.ffmpeg.org/ffmpeg.git
+###########################################################
+
+# install libopus
+cd $PreFix_Dir/src
+curl -O http://downloads.xiph.org/releases/opus/opus-1.0.3.tar.gz
+tar xzvf opus-1.0.3.tar.gz
+cd opus-1.0.3
+./configure --prefix="$PreFix_Dir" --disable-shared
+make
+make install
+make distclean
+
+####
+
+###########################################################
+# EXTRA @ xyz 2 // -> x265
+# original: git://source.ffmpeg.org/ffmpeg.git
+###########################################################
+cd $PreFix_Dir/src
+hg clone https://bitbucket.org/multicoreware/x265
+cd x265/build/linux
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PreFix_Dir" -DENABLE_SHARED:bool=off ../../source
+make
+make install
+####
+
+
+###########################################################
+# EXTRA @ xyz 3 // -> fdk-aac
+# original: git://source.ffmpeg.org/ffmpeg.git
+###########################################################
+
+# install libfdk_aac
+cd $PreFix_Dir/src
+git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
+cd fdk-aac
+autoreconf -fiv
+./configure --prefix="$PreFix_Dir" --disable-shared
+make
+make install
+make distclean
+
+
+###########################################################
+
+
+###########################################################
+# FFMPEG @ 9079e99d2c462ec7ef2e89d9e77ee6c3553dacce
+# original: git://source.ffmpeg.org/ffmpeg.git
+###########################################################
 # install FFmpeg
-cd ~/ffmpeg_sources
+cd $PreFix_Dir/src
 git clone --depth 1 git://source.ffmpeg.org/ffmpeg
 cd ffmpeg
-PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
+PKG_CONFIG_PATH="$PreFix_Dir/lib/pkgconfig"
 export PKG_CONFIG_PATH
-./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/bin" --extra-libs="-ldl" --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libmp3lame --enable-libopus --enable-libvorbis --enable-libvpx --enable-libx264
+./configure --prefix="$PreFix_Dir" --extra-cflags="-I$PreFix_Dir/include" --extra-ldflags="-L$PreFix_Dir/lib" --bindir="$PreFix_Dir/bin" \
+ --enable-gpl --enable-nonfree --enable-libfdk_aac --enable-libmp3lame --enable-libopus --enable-libvorbis \
+ --enable-libvpx --enable-libx264 \
+ #--extra-libs="-ldl"  \
+ --enable-libxvid --enable-libtheora --incdir=$PreFix_Dir/include  --enable-libfreetype  --enable-libx265 --libdir=$PreFix_Dir/lib \
+ --enable-libass --pkg-config=pkg-config 
+ 
 make
 make install
 make distclean
 hash -r
 . ~/.bash_profile
+export PATH="$PreFix_Dir/bin:$PATH"
+export LD_LIBRARY_PATH="$PreFix_Dir/lib:$LD_LIBRARY_PATH"
+
+
+
+
+
+#   --enable-avfilter \
+#--enable-pthreads  \
+ #--disable-mmx --enable-shared  \
+
+# --enable-version3 --enable-pic \
+# --pkg-config-flags="--static" 
+
+
+
+
+
+
+
+
+
