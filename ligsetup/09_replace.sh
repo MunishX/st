97b174b417c5b1137806678b3ed1a6dad816e3c1
munishgaurav5 <munishgaurav5@gmail.com>
2017-08-02 00:29:30 +0530

    Update 09_replace.sh

--- ligsetup/09_replace.sh
+++ ligsetup/09_replace.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 rm -rf /etc/lighttpd/lighttpd.old
-rm -rf /etc/lighttpd/conf.d/fastcgi.old
 rm -rf /etc/lighttpd/modules.old
+rm -rf /etc/lighttpd/conf.d/fastcgi.old
+rm -rf /etc/lighttpd/conf.d/cgi.old
+rm -rf /etc/lighttpd/conf.d/access_log.old
 
-mv  /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.old
-mv /etc/lighttpd/conf.d/fastcgi.conf /etc/lighttpd/conf.d/fastcgi.old
+mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.old
 mv /etc/lighttpd/modules.conf /etc/lighttpd/modules.old
+mv /etc/lighttpd/conf.d/fastcgi.conf /etc/lighttpd/conf.d/fastcgi.old
+mv /etc/lighttpd/conf.d/cgi.conf /etc/lighttpd/conf.d/cgi.old
+mv /etc/lighttpd/conf.d/access_log.conf /etc/lighttpd/conf.d/access_log.old
 
 #### LIGHTTPD CONFIG
 wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/ligcnf -O /etc/lighttpd/lighttpd.conf
+
+#### MODULES CONFIG
+wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/modcnf -O /etc/lighttpd/modules.conf
 
 #### FASTCGI CONFIG
 wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/fgicnf -O /etc/lighttpd/conf.d/fastcgi.conf
@@ -17,6 +24,6 @@ wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/fgicnf -O /
 #### CGI CONFIG
 wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/cgi -O /etc/lighttpd/conf.d/cgi.conf
 
-#### MODULES CONFIG
-wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/modcnf -O /etc/lighttpd/modules.conf
+#### ACCESS_LOG CONFIG
+wget https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/alog -O /etc/lighttpd/conf.d/access_log.conf
 
