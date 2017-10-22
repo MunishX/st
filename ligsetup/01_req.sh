d3bd63e3e1d4d27f749f70fce0b139f7e3e8fb8c
munishgaurav5 <munishgaurav5@gmail.com>
2017-10-22 22:53:20 +0530

    Update 01_req.sh

--- ligsetup/01_req.sh
+++ ligsetup/01_req.sh
@@ -10,13 +10,13 @@ service iptables stop
 chkconfig iptables off
 
 yum -y update
-yum -y install nano wget curl net-tools lsof bzip2 zip unzip rar unrar epel-release git sudo make cmake GeoIP sed at ant iotop iftop hdparm lsblk
+yum -y install nano wget curl net-tools lsof bzip2 zip unzip rar unrar epel-release git sudo make cmake GeoIP sed at ant iotop hdparm lsblk
 
 yum -y update
 
 sudo yum -y groupinstall "Development Tools"
 sudo yum -y install gcc gcc-c++ pcre pcre-devel zlib zlib-devel mailx expect imake lsof autoconf nc ca-certificates libedit-devel make automake expat-devel perl-libwww-perl perl-Crypt-SSLeay perl-Net-SSLeay tree virt-what cmake openssl-devel net-tools systemd-devel libdb-devel libxslt-devel gd gd-devel perl-ExtUtils-Embed patch sysstat libtool bind-utils libXext-devel cyrus-sasl-devel glib2 glib2-devel openssl ncurses-devel bzip2 bzip2-devel flex bison libcurl-devel which libevent libevent-devel libgcj gettext-devel vim-minimal nano cairo-devel libxml2-devel libxml2 libpng-devel freetype freetype-devel libart_lgpl-devel  GeoIP-devel gperftools-devel libicu libicu-devel aspell gmp-devel aspell-devel libtidy libtidy-devel readline-devel iptables* coreutils libedit-devel enchant enchant-devel pam-devel git perl-ExtUtils perl-ExtUtils-MakeMaker perl-Time-HiRes openldap openldap-devel curl curl-devel diffutils libc-client libc-client-devel numactl lsof pkgconfig gdbm-devel tk-devel bluez-libs-devel
-sudo yum -y install unzip zip rar unrar rsync psmisc syslog-ng-libdbi mediainfo
+sudo yum -y install unzip zip rar unrar rsync psmisc syslog-ng-libdbi mediainfo iftop
 
 
 ### RAR
