9252573bd20cb939a5e3d116bab10dfa406105c2
munishgaurav5 <munishgaurav5@gmail.com>
2018-03-23 14:28:26 +0530

    Update ff_installer.sh

--- ff_installer.sh
+++ ff_installer.sh
@@ -2,7 +2,7 @@
 
 ## MG New
 
-# cd /tmp && wget https://github.com/munishgaurav5/st/raw/master/ff_installer.sh && chmod 777 ff_installer.sh && ./ff_installer.sh
+# cd /tmp && rm -rf ff_* && wget https://github.com/munishgaurav5/st/raw/master/ff_installer.sh && chmod 777 ff_installer.sh && ./ff_installer.sh
 # cd /tmp && rm -rf ff_* && wget https://github.com/munishgaurav5/st/raw/master/ff_installer.sh && chmod 777 ff_installer.sh && nohup ./ff_installer.sh > ff_log.txt &
 
 start_time=`date +%s`
