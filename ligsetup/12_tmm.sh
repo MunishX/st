4ce5c4f06583d9baba36d235fda10206f1e219b2
munishgaurav5 <munishgaurav5@gmail.com>
2018-07-17 17:29:44 +0530

    Update 12_tmm.sh

--- ligsetup/12_tmm.sh
+++ ligsetup/12_tmm.sh
@@ -204,16 +204,15 @@ app_version=${mvvx}.${mvvy}.${mvvz}
 app_peer=${oppx}.${oppy}.${oppz}
 
 #### EDIT ####
-sed -i '/#define TR_NAME "Transmission"/a #define FXXTL_VERSION "APP_VERSION_PLACEHOLDER"' libtransmission/session.h
-sed -i '/#define TR_NAME "Transmission"/a #define FXXTL_NAME "APP_NAME_PLACEHOLDER"' libtransmission/session.h
-sed -i 's%^.*set(TR_PEER_ID_PREFIX ".*%set(TR_PEER_ID_PREFIX "APP_PEER_PLACEHOLDER")%' CMakeLists.txt
-sed -i 's%^.*curl_easy_setopt (e, CURLOPT_USERAGENT.*%  curl_easy_setopt (e, CURLOPT_USERAGENT, FXXTL_NAME "/" FXXTL_VERSION);%' libtransmission/web.c
-#sed -i 's%^.*curl_easy_setopt (e, CURLOPT_USERAGENT.*%curl_easy_setopt (e, CURLOPT_USERAGENT, FTL_NAME "/" SHORT_VERSION_STRING);%' configure.ac
+#sed -i '/#define TR_NAME "Transmission"/a #define FXXTL_VERSION "APP_VERSION_PLACEHOLDER"' libtransmission/session.h
+#sed -i '/#define TR_NAME "Transmission"/a #define FXXTL_NAME "APP_NAME_PLACEHOLDER"' libtransmission/session.h
+#sed -i 's%^.*set(TR_PEER_ID_PREFIX ".*%set(TR_PEER_ID_PREFIX "APP_PEER_PLACEHOLDER")%' CMakeLists.txt
+#sed -i 's%^.*curl_easy_setopt (e, CURLOPT_USERAGENT.*%  curl_easy_setopt (e, CURLOPT_USERAGENT, FXXTL_NAME "/" FXXTL_VERSION);%' libtransmission/web.c
+####sed -i 's%^.*curl_easy_setopt (e, CURLOPT_USERAGENT.*%curl_easy_setopt (e, CURLOPT_USERAGENT, FTL_NAME "/" SHORT_VERSION_STRING);%' configure.ac
 
-sed -i "s/APP_NAME_PLACEHOLDER/$app_name/" libtransmission/session.h
-sed -i "s/APP_VERSION_PLACEHOLDER/$app_version/" libtransmission/session.h
-sed -i "s/APP_PEER_PLACEHOLDER/$app_peer/" CMakeLists.txt
-
+#sed -i "s/APP_NAME_PLACEHOLDER/$app_name/" libtransmission/session.h
+#sed -i "s/APP_VERSION_PLACEHOLDER/$app_version/" libtransmission/session.h
+#sed -i "s/APP_PEER_PLACEHOLDER/$app_peer/" CMakeLists.txt
 ##############
 
 ./configure --prefix=/usr
