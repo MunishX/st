8e4064e995ab8d6b2ce4ffc16bd69b386429e9a3
munishgaurav5 <munishgaurav5@gmail.com>
2018-07-17 20:18:53 +0530

    Update 01_req.sh

--- ligsetup/01_req.sh
+++ ligsetup/01_req.sh
@@ -16,7 +16,7 @@ yum -y update
 
 sudo yum -y groupinstall "Development Tools"
 sudo yum -y install gcc gcc-c++ pcre pcre-devel zlib zlib-devel mailx expect imake lsof autoconf nc ca-certificates libedit-devel make automake expat-devel perl-libwww-perl perl-Crypt-SSLeay perl-Net-SSLeay tree virt-what cmake openssl-devel net-tools systemd-devel libdb-devel libxslt-devel gd gd-devel perl-ExtUtils-Embed patch sysstat libtool bind-utils libXext-devel cyrus-sasl-devel glib2 glib2-devel openssl ncurses-devel bzip2 bzip2-devel flex bison libcurl-devel which libevent libevent-devel libgcj gettext-devel vim-minimal nano cairo-devel libxml2-devel libxml2 libpng-devel freetype freetype-devel libart_lgpl-devel  GeoIP-devel gperftools-devel libicu libicu-devel aspell gmp-devel aspell-devel libtidy libtidy-devel readline-devel iptables* coreutils libedit-devel enchant enchant-devel pam-devel git perl-ExtUtils perl-ExtUtils-MakeMaker perl-Time-HiRes openldap openldap-devel curl curl-devel diffutils libc-client libc-client-devel numactl lsof pkgconfig gdbm-devel tk-devel bluez-libs-devel
-sudo yum -y install unzip zip rar unrar rsync psmisc syslog-ng-libdbi mediainfo iftop
+sudo yum -y install unzip zip rar unrar rsync psmisc syslog-ng-libdbi mediainfo iftop help2man
 
 ### RAR
 cd /tmp
