3fa90cfb45830d351f42457403ae167c57f9fc69
munishgaurav5 <munishgaurav5@gmail.com>
2018-07-31 01:44:09 +0530

    Update 12_tmm.sh

--- ligsetup/12_tmm.sh
+++ ligsetup/12_tmm.sh
@@ -192,18 +192,20 @@ nmmw=Fas
 nmmx=tTor
 nmmy=rentLe
 nmmz=ech
+
 mvvx=6
 mvvy=1
 mvvz=3
-oppx=FA
-oppy=ST
-oppz=TORR
+
+oppy=FA
+oppz=ST
+
 
 app_name=${nmmw}${nmmx}${nmmy}${nmmz}
 app_version=${mvvx}.${mvvy}.${mvvz}
 #app_peer=${oppx}.${oppy}.${oppz}
 #app_peer=${nmmw}${nmmx}${nmmy}${nmmz}v${mvvx}.${mvvy}.${mvvz}
-app_peer=--${oppx}${oppy}--
+app_peer=--${oppy}${oppz}--
 
 #### EDIT ####
 sed -i '/#define TR_NAME "Transmission"/a #define FXXTL_VERSION "APP_VERSION_PLACEHOLDER"' libtransmission/session.h
@@ -211,7 +213,7 @@ sed -i '/#define TR_NAME "Transmission"/a #define FXXTL_NAME "APP_NAME_PLACEHOLD
 sed -i 's%^.*curl_easy_setopt (e, CURLOPT_USERAGENT.*%  curl_easy_setopt (e, CURLOPT_USERAGENT, FXXTL_NAME "/" FXXTL_VERSION);%' libtransmission/web.c
 sed -i 's%^.*define MY_REALM.*%#define MY_REALM "APP_NAME_PLACEHOLDER APP_VERSION_PLACEHOLDER"%' libtransmission/rpc-server.c
 
-sed -i 's%^.*define PEERID_PREFIX.*%#define PEERID_PREFIX             "APP_PEER_PLACEHOLDER"%' libtransmission/version.h.in
+#sed -i 's%^.*define PEERID_PREFIX.*%#define PEERID_PREFIX             "APP_PEER_PLACEHOLDER"%' libtransmission/version.h.in
 
 
 sed -i "s/APP_NAME_PLACEHOLDER/$app_name/" libtransmission/session.h
@@ -222,7 +224,7 @@ sed -i "s/APP_VERSION_PLACEHOLDER/$app_version/" libtransmission/rpc-server.c
 sed -i "s/TR_NAME/FXXTL_NAME/" libtransmission/peer-msgs.c
 sed -i "s/USERAGENT_PREFIX/FXXTL_VERSION/" libtransmission/peer-msgs.c
 
-sed -i "s/APP_PEER_PLACEHOLDER/$app_peer/" libtransmission/version.h.in
+#sed -i "s/APP_PEER_PLACEHOLDER/$app_peer/" libtransmission/version.h.in
 
 ##############
 
