662364413ae967fe5c4740526592b714d0e6cfcf
munishgaurav5 <munishgaurav5@gmail.com>
2019-03-05 13:40:17 +0530

    Update ff_installer.sh

--- ff_installer.sh
+++ ff_installer.sh
@@ -5,7 +5,7 @@
 # cd /tmp && rm -rf ff_* && wget https://github.com/munishgaurav5/st/raw/master/ff_installer.sh && chmod 777 ff_installer.sh && ./ff_installer.sh
 # cd /tmp && rm -rf ff_* && wget https://github.com/munishgaurav5/st/raw/master/ff_installer.sh && chmod 777 ff_installer.sh && nohup ./ff_installer.sh > ff_log.txt &
 
-# cd /tmp && yum install wget nano zip unzip -y && rm -rf ff_* && wget https://github.com/munishgaurav5/st/raw/master/ff_installer.sh && chmod 777 ff_installer.sh 
+# cd /tmp && yum install wget -y && rm -rf ff_* && wget https://github.com/munishgaurav5/st/raw/master/ff_installer.sh && chmod 777 ff_installer.sh 
 # ./ff_installer.sh > ffout.txt 2>&1 &
 # watch -n 2 tail -n 30 ffout.txt
 
