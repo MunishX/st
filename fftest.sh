f02664b43e76e1ec1b2b58696a45215b45cf17af
munishgaurav5 <munishgaurav5@gmail.com>
2019-03-17 12:55:01 +0530

    Update fftest.sh

--- fftest.sh
+++ fftest.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 Root_Dir="/usr/local"
-
 FF_Source="$Root_Dir/ffmpeg_sources"
 FF_Build="$Root_Dir/ffmpeg_build"
 
@@ -9,49 +8,115 @@ PreFix_Dir="$FF_Build"
 
 BUILD_DIR=$FF_Source
 TARGET_DIR=$FF_Build
-BIN_DIR="$Root_Dir/bin"
+BIN_DIR="$Root_Dir/ffbin"
 
 mkdir -p "$BUILD_DIR" "$TARGET_DIR" "$BIN_DIR"
 
+create_dir(){
+  echo "removing and creating dirs $1 $2 $3"
+  rm -rf  $1 $2 $3
+  mkdir -p $1 $2 $3
+}
+
+
+download(){
+  filename="$1"
+  if [ ! -z "$2" ];then
+    filename="$2"
+  fi
+  ../download.pl "$DOWNLOAD_DIR" "$1" "$filename" "$3" "$4"
+  #disable uncompress
+  REPLACE="$rebuild" CACHE_DIR="$DOWNLOAD_DIR" ../fetchurl "http://cache/$filename"
+}
 
 echo "#### FFmpeg static build ####"
 
-#this is our working directory
-cd $BUILD_DIR
 
 #cd $FF_Source
 #curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
 #tar xzvf yasm-1.3.0.tar.gz
 #cd yasm-1.3.0
 
-http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
-https://www.nasm.us/pub/nasm/releasebuilds/2.14/nasm-2.14.tar.gz
-https://github.com/openssl/openssl/archive/OpenSSL_1_1_1b.tar.gz
+yasm_url="http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz"
+nasm_url="https://www.nasm.us/pub/nasm/releasebuilds/2.14/nasm-2.14.tar.gz"
+openssl_url="https://github.com/openssl/openssl/archive/OpenSSL_1_1_1b.tar.gz"
 
-https://github.com/madler/zlib/archive/v1.2.11.tar.gz
-http://download.videolan.org/pub/videolan/x264/snapshots/last_x264.tar.bz2
-https://bitbucket.org/multicoreware/x265/downloads/x265_3.0.tar.gz
-https://github.com/mstorsjo/fdk-aac/archive/v2.0.0.tar.gz
-https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.3.1.tar.bz2
-https://github.com/fribidi/fribidi/releases/download/v1.0.5/fribidi-1.0.5.tar.bz2
-https://github.com/libass/libass/releases/download/0.14.0/libass-0.14.0.tar.gz
-http://download.videolan.org/pub/contrib/lame/lame-3.100.tar.gz
+zlib_url="https://github.com/madler/zlib/archive/v1.2.11.tar.gz"
+x264_url="http://download.videolan.org/pub/videolan/x264/snapshots/last_x264.tar.bz2"
+x265_url="https://bitbucket.org/multicoreware/x265/downloads/x265_3.0.tar.gz
+fakaac_url="https://github.com/mstorsjo/fdk-aac/archive/v2.0.0.tar.gz"
+harfbuzz_url="https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.3.1.tar.bz2"
+fribidi_url="https://github.com/fribidi/fribidi/releases/download/v1.0.5/fribidi-1.0.5.tar.bz2"
+ass_url="https://github.com/libass/libass/releases/download/0.14.0/libass-0.14.0.tar.gz"
+lame_url="http://download.videolan.org/pub/contrib/lame/lame-3.100.tar.gz"
 # http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
 
-https://github.com/xiph/opus/archive/v1.3.tar.gz
-https://github.com/webmproject/libvpx/archive/v1.8.0.tar.gz
-https://rtmpdump.mplayerhq.hu/download/rtmpdump-2.3.tgz
-https://sourceforge.net/projects/soxr/files/soxr-0.1.3-Source.tar.xz
+opus_url="https://github.com/xiph/opus/archive/v1.3.tar.gz"
+vpx_url="https://github.com/webmproject/libvpx/archive/v1.8.0.tar.gz"
+rtmpdump_url="https://rtmpdump.mplayerhq.hu/download/rtmpdump-2.3.tgz"
+soxr_url="https://sourceforge.net/projects/soxr/files/soxr-0.1.3-Source.tar.xz"
 # https://excellmedia.dl.sourceforge.net/project/soxr/soxr-0.1.3-Source.tar.xz
 
-https://github.com/georgmartius/vid.stab/archive/v1.1.0.tar.gz
-https://github.com/sekrit-twc/zimg/archive/release-2.8.tar.gz
-https://github.com/uclouvain/openjpeg/releases/download/v2.3.0/openjpeg-v2.3.0-linux-x86_64.tar.gz
-https://github.com/webmproject/libwebp/archive/v1.0.2.tar.gz
-https://github.com/xiph/vorbis/archive/v1.3.6.tar.gz
-https://github.com/xiph/ogg/releases/download/v1.3.3/libogg-1.3.3.tar.gz
-https://github.com/xiph/speex/archive/Speex-1.2.0.tar.gz
-http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
+vidstab_url="https://github.com/georgmartius/vid.stab/archive/v1.1.0.tar.gz"
+zimg_url="https://github.com/sekrit-twc/zimg/archive/release-2.8.tar.gz"
+openjpeg_url="https://github.com/uclouvain/openjpeg/releases/download/v2.3.0/openjpeg-v2.3.0-linux-x86_64.tar.gz"
+webp_url="https://github.com/webmproject/libwebp/archive/v1.0.2.tar.gz"
+vorbis_url="https://github.com/xiph/vorbis/archive/v1.3.6.tar.gz"
+ogg_url="https://github.com/xiph/ogg/releases/download/v1.3.3/libogg-1.3.3.tar.gz"
+speex_url="https://github.com/xiph/speex/archive/Speex-1.2.0.tar.gz"
+ffmpeg_url="http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2"
+
+
+#this is our working directory
+cd $BUILD_DIR
+
+#tar.gz
+#tar xzvf yasm${ext}
+
+url="_url"
+
+ext=".tar.gz"
+wget -O yasm${ext} $yasm${url}
+wget -O nasm${ext} $nasm${url}
+wget -O openssl${ext} $openssl${url}
+wget -O zlib${ext} $zlib${url}
+
+ext=".tar.bz2"
+wget -O x264${ext} $x264${url}
+
+ext=".tar.gz"
+wget -O x265${ext} $x265${url}
+wget -O fakaac${ext} $fakaac${url}
+
+ext=".tar.bz2"
+wget -O harfbuzz${ext} $harfbuzz${url}
+wget -O fribidi${ext} $fribidi${url}
+
+ext=".tar.gz"
+wget -O ass${ext} $ass${url}
+wget -O lame${ext} $lame${url}
+wget -O opus${ext} $opus${url}
+wget -O vpx${ext} $vpx${url}
+
+ext=".tgz"
+wget -O rtmpdump${ext} $rtmpdump${url}
+
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+wget -O yasm${ext} $nasm${url}
+
+
+
+tar xzvf yasm.tar.gz
 
 
 if [ $is_x86 -eq 1 ]; then
