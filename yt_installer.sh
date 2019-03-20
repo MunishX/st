2695411e3da37a8e95cae4f9a5535c7864d6afad
munishgaurav5 <munishgaurav5@gmail.com>
2019-03-20 21:08:48 +0530

    Update yt_installer.sh

--- yt_installer.sh
+++ yt_installer.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
 
-## YT-DL install
-cd /tmp
-rm -rf /usr/local/bin/youtube-dl
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
-sudo youtube-dl -U
-echo "YT-DL Install Complete"
-echo ""
-
 ## PH-JS
 cd /tmp
 rm -rf phantomjs-2.1.1-linux-x86_64*
@@ -19,5 +10,23 @@ mv phantomjs-2.1.1-linux-x86_64/bin/* /usr/local/bin/
 echo "PH-JS Install Complete"
 echo ""
 
-chmod 777 /usr/local/bin/*
+
+## YT-DL install
+cd /tmp
+rm -rf /usr/local/bin/youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+echo "YT-DL Install Complete"
+echo ""
+
+
+#chmod 777 /usr/local/bin/*
+
+echo ""
+echo "Phantomjs Version : "
+phantomjs -v
+echo ""
+echo "Youtube-dl Version : "
+youtube-dl -U
+
 echo "--- DONE ---"
