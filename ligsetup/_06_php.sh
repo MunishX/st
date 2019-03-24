c2b250a09dfdcc9291bacefecd2a60512427cf16
munishgaurav5 <munishgaurav5@gmail.com>
2019-03-24 16:17:04 +0530

    Update _06_php.sh

--- ligsetup/_06_php.sh
+++ ligsetup/_06_php.sh
@@ -1,54 +1,80 @@
 #!/bin/bash
 
+## PHP 7.2
 # add PHP 7 repo
+
+PHP_V='php72'
+
 cd /tmp
-wget https://centos7.iuscommunity.org/ius-release.rpm
-wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
-rpm -ivh ius-release.rpm remi-release-7.rpm
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+wget https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm webtatic-release.rpm
 yum -y update
 
 ####
 
 # PHP 7 Install
-yum -y install php70-php-bcmath php70-php-mysql php70-php-devel php70-php-fpm php70-php-gd php70-php-intl php70-php-imap php70-php-mbstring php70-php-mcrypt php70-php-mysqlnd php70-php-opcache php70-php-pdo php70-php-pear php70-php-soap php70-php-xml php70-php-xmlrpc
-yum -y install php70-php-pecl-uploadprogress php70-php-pecl-zip
-yum -y install php70-php-memcached php70-php-memcache php70-php-apcu* memcached
 yum -y install libevent libevent-devel
+yum -y install php72w* --exclude=php72w-mysql --exclude=php72w-pecl-mongodb
 yum -y update
 
-## PHP-PHALCON + PHP-MongoDB
-curl -s https://packagecloud.io/install/repositories/phalcon/stable/script.rpm.sh | sudo bash
-yum -y install php70-php-phalcon php70-php-pecl-mongodb
 
 ## PHP FIX + DATE 
-echo "cgi.fix_pathinfo=1" >> /etc/opt/remi/php70/php.ini
-echo "date.timezone = UTC" >> /etc/opt/remi/php70/php.ini
+echo "cgi.fix_pathinfo=1" >> /etc/php.ini
+echo "date.timezone = UTC" >> /etc/php.ini
 
-## PHP70 to PHP link
-ln -s /usr/bin/php70 /usr/bin/php
-ln -s /opt/remi/php70/root/usr/sbin/php-fpm /usr/bin/php-fpm
+
+## php7x to PHP link
+#ln -s /usr/bin/${PHP_V} /usr/bin/php
+ln -s /usr/sbin/php-fpm /usr/bin/php-fpm
 
 ## SWITCH OFF EXPOSE PHP
-sed -i "s/^.*expose_php =.*/expose_php = Off/" /etc/opt/remi/php70/php.ini
-sed -i "s/^.*upload_max_filesize =.*/upload_max_filesize = 20M/" /etc/opt/remi/php70/php.ini
+sed -i "s/^.*expose_php =.*/expose_php = Off/" /etc/php.ini
+sed -i "s/^.*mail.add_x_header =.*/mail.add_x_header = Off/" /etc/php.ini
+sed -i "s/^.*upload_max_filesize =.*/upload_max_filesize = 20M/" /etc/php.ini
 
 ####
 
 ### MEMCACHED CONFIG
-yum -y install memcached iftop
+yum -y install memcached redis
 
 echo '
 PORT="11211"
 USER="memcached"
 MAXCONN="1024"
 CACHESIZE="1024"
-OPTIONS="localhost"
+OPTIONS="-l 127.0.0.1"
 ' > /etc/sysconfig/memcached
 
 #####
 
 #### OPCACHE CONFIG
-sed -i "s/^.*opcache.memory_consumption=.*/opcache.memory_consumption=999/" /etc/opt/remi/php70/php.d/10-opcache.ini
-sed -i "s/^.*opcache.max_accelerated_files=.*/opcache.max_accelerated_files=94000/" /etc/opt/remi/php70/php.d/10-opcache.ini
+sed -i "s/^.*opcache.memory_consumption=.*/opcache.memory_consumption=999/" /etc/php.d/opcache.ini
+sed -i "s/^.*opcache.max_accelerated_files=.*/opcache.max_accelerated_files=94000/" /etc/php.d/opcache.ini
+
+sed -i "s/^.*opcache.memory_consumption=.*/opcache.memory_consumption=999/" /etc/php-zts.d/opcache.ini
+sed -i "s/^.*opcache.max_accelerated_files=.*/opcache.max_accelerated_files=94000/" /etc/php-zts.d/opcache.ini
 ####
+
+
+cd /tmp
+rm -rf pthreads*
+wget -O pthreads.zip https://github.com/krakjoe/pthreads/archive/v3.2.0.zip
+unzip pthreads.zip
+rm -rf pthreads.zip
+cd pthreads*/
+zts-phpize
+./configure --with-php-config=/usr/bin/zts-php-config
+make
+
+cp modules/pthreads.so /usr/lib64/php-zts/modules/.
+echo 'extension=pthreads.so' > /etc/php-zts.d/pthreads.ini
+
+zts-php -i | grep -i thread
+
+# /usr/bin/zts-php
+# /usr/bin/php
+# /usr/bin/php-fpm
+
+# /usr/sbin/php-fpm
 
