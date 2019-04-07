c1da43176937a19d4bdc3d3c4dba3b7a1ee9f070
munishgaurav5 <munishgaurav5@gmail.com>
2019-04-07 20:24:54 +0530

    Update yt_installer.sh

--- yt_installer.sh
+++ yt_installer.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-
+#phantomjs and youtube-dl install
 
 yum -y install wget curl nano bzip2 zip unzip fontconfig
 
 ## PH-JS
 cd /tmp
 rm -rf /usr/local/bin/phantomjs
-rm -rf phantomjs-2.1.1-linux-x86_64*
+rm -rf phantomjs*
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2
 mv phantomjs-2.1.1-linux-x86_64/bin/* /usr/local/bin/
+rm -rf phantomjs*
 echo "PH-JS Install Complete"
 echo ""
 
@@ -38,5 +39,6 @@ echo ""
 echo "Youtube-dl Version : "
 youtube-dl -U
 echo ""
+echo "#### DONE ####"
 echo ""
 
