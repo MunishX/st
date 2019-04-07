ba3df940a1822407c19e598bc1eca6f829010b3d
munishgaurav5 <munishgaurav5@gmail.com>
2019-04-07 20:21:27 +0530

    Update axel_n_aria2.sh

--- axel_n_aria2.sh
+++ axel_n_aria2.sh
@@ -1,26 +1,31 @@
-yum -y install wget curl nano bzip2 zip unzip fontconfig gcc 
+#!/usr/bin/env bash
+#axel and aria2 install
 
+yum -y install wget curl nano bzip2 zip unzip fontconfig gcc 
 
 ## axel
 cd /tmp
 rm -rf axel*
 wget https://github.com/axel-download-accelerator/axel/releases/download/v2.16.1/axel-2.16.1.tar.gz
-tar xzvf axel-2.16.1.tar.gz
-rm -rf axel-2.16.1.tar.gz
-cd axel-*/
+tar xzvf axel*.tar.gz
+rm -rf axel*.tar.gz
+cd axel*/
 ./configure
 make
 make install
-
+cd /tmp
+rm -rf axel*
 
 ## aria2
 cd /tmp
 rm -rf aria2*
 wget https://github.com/q3aql/aria2-static-builds/releases/download/v1.34.0/aria2-1.34.0-linux-gnu-64bit-build1.tar.bz2
-tar jxvf aria2-*.tar.bz2
-rm -rf jxvf aria2-*.tar.bz2
-cd aria2-*/
+tar jxvf aria2*.tar.bz2
+rm -rf jxvf aria2*.tar.bz2
+cd aria2*/
 make PREFIX=/usr/local
+cd /tmp
+rm -rf aria2*
 
 
 
