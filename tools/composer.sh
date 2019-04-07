3c8c45ab7f71389f64062e3e6b81cdd9e4c1ee1d
munishgaurav5 <munishgaurav5@gmail.com>
2019-04-07 20:11:03 +0530

    Update and rename composer.sh to tools/composer.sh

--- tools/composer.sh
+++ tools/composer.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
-
 #composer install
 
 yum -y install wget unzip
 
+cd /tmp
+rm -rf composer*
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 mkdir -p /home/admin/bin/
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 php composer-setup.php --install-dir=/home/admin/bin --filename=composer
-
+cd /tmp
+rm -rf composer*
 chown -R admin:admin /home/admin/bin/*
 chmod 777 /home/admin/bin/*
+composer -v
 
 echo ""
-echo "#### FINISH ####"
+echo "#### Composer DONE ####"
 echo ""
