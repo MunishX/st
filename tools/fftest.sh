ea10289de16bc9d689734278ea1b2409041e91fd
munishgaurav5 <munishgaurav5@gmail.com>
2019-09-04 00:14:35 +0530

    Update fftest.sh

--- tools/fftest.sh
+++ tools/fftest.sh
@@ -81,17 +81,6 @@ fi
 
 
 
-cd /tmp
-rm -rf cmake*
-#wget https://cmake.org/files/v3.14/cmake-3.14.0.tar.gz
-wget https://cmake.org/files/v3.15/cmake-3.15.0.tar.gz
-tar zxvf cmake-3.*
-cd cmake-3.*/
-./bootstrap --prefix=/usr/local
-make -j$(nproc)
-make install
-cmake --version
-
 
 ##############
 ### FFMPEG ###
