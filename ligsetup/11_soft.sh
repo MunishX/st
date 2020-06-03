a42baf0a585a721259d351c2d8d9c23f0b370b0a
munishgaurav5 <munishgaurav5@gmail.com>
2020-06-03 20:32:34 +0530

    Update 11_soft.sh

--- ligsetup/11_soft.sh
+++ ligsetup/11_soft.sh
@@ -94,7 +94,8 @@ rm -rf mem.zip
 #wget https://files.phpmyadmin.net/phpMyAdmin/4.7.7/phpMyAdmin-4.7.7-english.zip
 #wget https://files.phpmyadmin.net/phpMyAdmin/4.7.8/phpMyAdmin-4.7.8-english.zip
 #wget https://files.phpmyadmin.net/phpMyAdmin/4.8.2/phpMyAdmin-4.8.2-english.zip
-wget https://files.phpmyadmin.net/phpMyAdmin/4.8.3/phpMyAdmin-4.8.3-english.zip
+#wget https://files.phpmyadmin.net/phpMyAdmin/4.8.3/phpMyAdmin-4.8.3-english.zip
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.5/phpMyAdmin-4.9.5-english.zip
 
 unzip phpMy*
 rm -rf phpMy*.zip
