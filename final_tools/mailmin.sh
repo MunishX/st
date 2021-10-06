239212f051fc38b957df92c00dd7985eeb5fe238
munishgaurav5 <munishgaurav5@gmail.com>
2021-10-06 21:35:23 +0530

    Update mailmin.sh

--- final_tools/mailmin.sh
+++ final_tools/mailmin.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# cd /tmp/ && rm -rf /tmp/mailmin.sh && wget https://github.com/munishgaurav5/st/raw/master/final_tools/mailmin.sh && chmod 777 /tmp/mailmin.sh && ./tmp/mailmin.sh 
+
 yum install sudo sed postfix -y
 sudo sed -i "s/^inet_protocols.*/inet_protocols = ipv4/" /etc/postfix/main.cf
 
