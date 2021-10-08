2efb6cba0e0ac7a3f5db2268ae4c900428c20ff1
munishgaurav5 <munishgaurav5@gmail.com>
2021-10-09 01:25:33 +0530

    Update 01_req.sh

--- ligsetup/01_req.sh
+++ ligsetup/01_req.sh
@@ -18,7 +18,7 @@ sudo yum -y groupinstall "Development Tools"
 sudo yum -y install gcc gcc-c++ pcre pcre-devel zlib zlib-devel mailx expect imake lsof autoconf nc ca-certificates libedit-devel make automake expat-devel perl-libwww-perl perl-Crypt-SSLeay perl-Net-SSLeay tree virt-what cmake openssl-devel net-tools systemd-devel libdb-devel libxslt-devel gd gd-devel perl-ExtUtils-Embed patch sysstat libtool bind-utils libXext-devel cyrus-sasl-devel glib2 glib2-devel openssl ncurses-devel bzip2 bzip2-devel flex bison libcurl-devel which libevent libevent-devel libgcj gettext-devel vim-minimal nano cairo-devel libxml2-devel libxml2 libpng-devel freetype freetype-devel libart_lgpl-devel  GeoIP-devel gperftools-devel libicu libicu-devel aspell gmp-devel aspell-devel libtidy libtidy-devel readline-devel iptables* coreutils libedit-devel enchant enchant-devel pam-devel git perl-ExtUtils perl-ExtUtils-MakeMaker perl-Time-HiRes openldap openldap-devel curl curl-devel diffutils libc-client libc-client-devel numactl lsof pkgconfig gdbm-devel tk-devel bluez-libs-devel
 sudo yum -y install unzip zip rar unrar rsync psmisc syslog-ng-libdbi mediainfo iftop help2man
 
-### RAR
+########## RAR #############
 echo ""
 echo "Installing latest RAR..."
 cd /tmp
@@ -30,6 +30,21 @@ make
 wget -O /etc/rarreg.key https://github.com/munishgaurav5/st/raw/master/ligsetup/replace/rarreg.key
 cd ..
 rm -rf rar*
+
+############# SQLITE3 ######################
+mv /usr/local/bin/sqlite3 /usr/local/bin/sqlite3_old
+
+cd /tmp
+wget https://www.sqlite.org/2021/sqlite-autoconf-3360000.tar.gz
+tar -xf sqlite*.tar.gz
+cd sqlite*/
+CFLAGS="-DSQLITE_ENABLE_COLUMN_METADATA=1" ./configure
+make
+make install
+
+sqlite3 --version
+ 
+############################################
 
 ########
 ## CROND : cronie
