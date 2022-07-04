e941e248f19616df47dec7c89a82f12cc6cc7d59
munishgaurav5 <munishgaurav5@gmail.com>
2022-07-04 22:13:58 +0530

    Create mail_tester_ubuntu.sh

---
+++ final_tools/mail_tester_ubuntu.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# for ubuntu or debian 
+
+# cd /tmp && sudo apt install wget -y && rm -rf /tmp/mail.sh && wget http://host.fastserver.me/tmp/mail.sh && chmod 777 mail.sh && bash /tmp/mail.sh
+
+sudo apt update -y
+sudo apt install -y nano wget curl sed openssl unzip zip  
+sudo apt install -y software-properties-common
+
+sudo wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+
+sudo echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo apt update -y
+
+sudo apt install -y php7.4 php7.4-cgi php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-imap php7.4-intl php7.4-json php7.4-ldap php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-soap php7.4-xml php7.4-zip php7.4-xmlrpc php-imagick php7.4-bcmath php-dompdf php-fpm php-mysql php7.4-memcache php7.4-memcached php7.4-geoip php7.4-gnupg  php7.4-fpm php7.4-bz2 php7.4-mbstring
+
+sleep 2
+
+php7.4 -v
+
+sleep 2
+
+
+# postfix install
+
+apt install sudo sed postfix -y
+sudo sed -i "s/^inet_protocols.*/inet_protocols = ipv4/" /etc/postfix/main.cf
+
+sudo postconf -e 'smtp_tls_security_level = may'
+sudo postconf -e 'smtpd_tls_security_level = may'
+
+# postfix reload
+service postfix restart
+
+echo "Min Postfix Configured Successfully..."
+
+
+#Apache install
+
+sudo apt install -y apache2 libapache2-mod-php
+rm -rf /var/www/html/up.php /var/www/html/PHPMailer*
+sudo wget -O /var/www/html/up.php https://github.com/dulldusk/phpfm/raw/master/index.php
+
+chown -R www-data:www-data /var/www/html
+
+
+# PHPMailer install
+
+cd /var/www/html
+wget http://host.fastserver.me/tmp/PHPMailer.zip
+#wget -O PHPMailer.zip https://github.com/PHPMailer/PHPMailer/archive/refs/tags/v6.6.3.zip
+unzip PHPMailer.zip
+chown -R www-data:www-data /var/www/html
+
+
+#hostname setup
+
+hostnamectl set-hostname host.fastserver.me
+
+echo ""
+echo "SETUP Configured Successfully..."
+echo ""
+echo ""
+echo "if need to change hostname, run: hostnamectl set-hostname host.fastserver.me "
+echo "set A record"
+echo "set SPF record"
+echo "set reverse IP Host at Network"
+echo ""
+echo " Then visit http://test16.fastserver.me/PHPMailer/src/test.php "
+echo ""
+
+
