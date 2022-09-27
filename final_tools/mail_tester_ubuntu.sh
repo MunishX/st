5f0d5bebce426e84e0ff3f7c2a0dddc494fd8576
munishgaurav5 <munishgaurav5@gmail.com>
2022-09-27 23:38:18 +0530

    Update mail_tester_ubuntu.sh

--- final_tools/mail_tester_ubuntu.sh
+++ final_tools/mail_tester_ubuntu.sh
@@ -8,10 +8,14 @@ sudo apt update -y
 sudo apt install -y nano wget curl sed openssl unzip zip  
 sudo apt install -y software-properties-common
 
-sudo wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+#sudo wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+#sudo echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+#sudo apt update -y
 
-sudo echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
-sudo apt update -y
+cd /tmp
+wget -O sury.sh https://packages.sury.org/php/README.txt
+chmod 777 sury.sh
+./sury.sh
 
 sudo apt install -y php7.4 php7.4-cgi php7.4-cli php7.4-common php7.4-curl php7.4-gd php7.4-imap php7.4-intl php7.4-json php7.4-ldap php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-soap php7.4-xml php7.4-zip php7.4-xmlrpc php-imagick php7.4-bcmath php-dompdf php-fpm php-mysql php7.4-memcache php7.4-memcached php7.4-geoip php7.4-gnupg  php7.4-fpm php7.4-bz2 php7.4-mbstring
 
@@ -57,18 +61,18 @@ chown -R www-data:www-data /var/www/html
 
 #hostname setup
 
-hostnamectl set-hostname host.fastserver.me
+hostnamectl set-hostname test10.fastserver.me
 
 echo ""
 echo "SETUP Configured Successfully..."
 echo ""
 echo ""
-echo "if need to change hostname, run: hostnamectl set-hostname host.fastserver.me "
+echo "if need to change hostname, run: hostnamectl set-hostname test10.fastserver.me "
 echo "set A record"
 echo "set SPF record"
 echo "set reverse IP Host at Network"
 echo ""
-echo " Then visit http://test16.fastserver.me/PHPMailer/src/test.php "
+echo " Then visit http://[IP]/PHPMailer/src/test.php or http://test10.fastserver.me/PHPMailer/src/test.php"
 echo ""
 
 
