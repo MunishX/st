c333916169b86b0229914bc815354f6b85538ceb
MunishX <munishgaurav5@gmail.com>
2023-11-16 15:40:46 +0530

    Create node_npm_nvm_install.sh

---
+++ tools/node_npm_nvm_install.sh
@@ -0,0 +1,19 @@
+
+cd /tmp
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+source ~/.bashrc
+nvm --version
+
+nvm install node
+
+node --version
+
+# nvm install 14
+# nvm install 16
+# nvm install 18
+# nvm install 20
+
+# nvm uninstall 20
+
+# nvm use 18
+# node --version
