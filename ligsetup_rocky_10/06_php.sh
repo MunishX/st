ccde78789f2d229fc209aaf14a09fc2d3002a3ec
MunishX <munishgaurav5@gmail.com>
2026-07-07 12:23:29 +0530

    Update 06_php.sh

--- ligsetup_rocky_10/06_php.sh
+++ ligsetup_rocky_10/06_php.sh
@@ -94,16 +94,30 @@ ln -s /opt/remi/${PHP_V}/root/usr/bin/php-cgi /usr/bin/php-cgi
 ####
 
 ### MEMCACHED CONFIG
-yum -y install memcached redis
+yum -y install memcached 
 
 echo '
 PORT="11211"
 USER="memcached"
 MAXCONN="1024"
 CACHESIZE="1024"
-OPTIONS="-l 127.0.0.1"
+OPTIONS="-l 127.0.0.1,::1"
 ' > /etc/sysconfig/memcached
 
+### REDIS / Valkey 
+# (Redis available till Rocky 9)
+# yum -y install redis 
+
+yum -y install valkey
+valkey-server --version
+
+systemctl enable valkey # --now
+systemctl start valkey
+systemctl status valkey
+
+systemctl is-active valkey
+systemctl is-enabled valkey
+valkey-cli ping
 #####
 
 #### OPCACHE CONFIG
