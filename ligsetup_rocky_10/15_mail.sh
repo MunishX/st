63ac3208c18ae2a5897e501dfedf8aca6000faad
MunishX <munishgaurav5@gmail.com>
2026-07-07 20:35:18 +0530

    Create 15_mail.sh

---
+++ ligsetup_rocky_10/15_mail.sh
@@ -0,0 +1,25 @@
+### Postfix Setup
+echo "Postfix setup starting..."
+sleep 5 
+
+yum install sudo sed postfix -y
+sudo sed -i "s/^inet_protocols.*/inet_protocols = ipv4/" /etc/postfix/main.cf
+sudo postconf -e 'smtp_tls_security_level = may'
+sudo postconf -e 'smtpd_tls_security_level = may'
+systemctl start postfix
+systemctl enable postfix
+systemctl status postfix --no-pager
+###
+
+echo ""
+echo " PostFix SETUP Configured Successfully..."
+echo ""
+echo ""
+echo "if need to change hostname, run: hostnamectl set-hostname test10.fastserver.me "
+echo "set A record"
+echo "set SPF record / update server ip in spf record"
+echo "set reverse IP Host at Network"
+echo ""
+echo " Then visit http://$(hostname)/host/PHPMailer/src/test.php or http://test10.fastserver.me/host/PHPMailer/src/test.php"
+echo ""
+
