85454820397974a6ea17095fa626688d26f24630
MunishX <munishgaurav5@gmail.com>
2026-07-08 20:39:04 +0530

    Update 00_install.sh

--- ligsetup_rocky_10/00_install.sh
+++ ligsetup_rocky_10/00_install.sh
@@ -167,7 +167,7 @@ sleep 10
 # Req Install and Update
 #------------------------------------------------------------------------------------
 
-wget ${BASE_URL}01_req.sh $BASE_URL
+wget ${BASE_URL}01_req.sh $NETWORK_INTERFACE_NAME $BASE_URL
 chmod 777 01_req.sh
 ./01_req.sh
 
