8c33b90bce6cc635ba883ddfac509fa1938c2a6c
MunishX <munishgaurav5@gmail.com>
2026-07-08 09:37:14 +0530

    Update 11_create_vhost.sh

--- ligsetup_rocky_10/11_create_vhost.sh
+++ ligsetup_rocky_10/11_create_vhost.sh
@@ -460,7 +460,7 @@ mkdir -p $user_root/$mydom/$php_add_head/{session,savedsession,wsdlcache,opcache
 
    #$server_stat
 
-  $HTTP["url"] =~ "^/host/" {
+  \$HTTP["url"] =~ "^/host/" {
     auth.backend = \"htpasswd\"
     auth.backend.htpasswd.userfile = \"$user_root/$mydom/.htpasswd\"
     auth.require = ( \"/host/\" =>
