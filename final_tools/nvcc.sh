d9dae0d784398a1cf6758f13b2d8c43531b36a3d
MunishX <munishgaurav5@gmail.com>
2026-07-21 22:24:56 +0530

    Update nvcc.sh

--- final_tools/nvcc.sh
+++ final_tools/nvcc.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
-detect_gcc(){
-# Extract the major version number directly
-GCC_VERSION=$(gcc -dumpversion | cut -d. -f1)
+detect_gcc() {
+    if gcc -dumpversion; then
+        echo "gcc found"
+    else
+        echo "gcc not found.., Install gcc 11+ to continue, Exiting.."
+        exit 1
+    fi
 
-# Check if the major version is greater than 7
-if [ "$GCC_VERSION" -lt 11 ]; then
-    echo "GCC version ($GCC_VERSION) is lower than 11. Upgrade gcc to 11+ . Exiting."
-    exit 1
-else
-    echo "GCC version ($GCC_VERSION) is 11 or above. [OK] continuing.."
-    echo "GCC [OK]"
-    echo ""
-fi
+    GCC_VERSION=$(gcc -dumpversion | cut -d. -f1)
+
+    if [ "$GCC_VERSION" -lt 11 ]; then
+        echo "GCC version ($GCC_VERSION) is lower than 11. Upgrade gcc to 11+ . Exiting."
+        exit 1
+    else
+        echo "GCC version ($GCC_VERSION) is 11 or above. continuing.."
+        echo "GCC [OK]"
+        echo ""
+    fi
 }
 
-detect_os(){
+detect_os() {
     os_type="NULL"
 
-    if [ -n "$(command -v yum)" ]
-    then
-    os_type="yum"
+    if [ -n "$(command -v yum)" ]; then
+        os_type="yum"
     fi
 
-    if [ -n "$(command -v apt-get)" ]
-    then
-    os_type="apt"
+    if [ -n "$(command -v apt-get)" ]; then
+        os_type="apt"
     fi
 
-    if [[ $os_type = "NULL" ]]
-    then
-    echo "[error] Un-Supported OS found. Exiting.. Supported OS are: Ubuntu, Debian, CentOS, Rocky, AmlaLinux, Redhat and Fedora."
-    exit 0
+    if [[ "$os_type" = "NULL" ]]; then
+        echo "[error] Un-Supported OS found. Exiting.."
+        exit 1
     else
-    echo "[info] Supported OS found. OS_Type: $os_type"
+        echo "[info] Supported OS found. OS_Type: $os_type"
     fi
 }
 
@@ -57,10 +59,10 @@ install_required_tools(){
 install_cuda_drivers(){
     if [[ "$os_type" == "yum" ]]; then
         echo "[info] Installing basic tools via yum..."
-        sudo dnf config-manager --add-repo https://nvidia.com
+        sudo dnf config-manager --add-repo 'https://nvidia.com'
         sudo dnf install cuda-drivers
         sudo dnf install cuda-toolkit
-        
+
         echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
         echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
         source ~/.bashrc
@@ -69,7 +71,7 @@ install_cuda_drivers(){
     if [[ "$os_type" == "apt" ]]; then
         echo "[info] Installing basic tools via apt..."
         apt-get -y install nano wget curl net-tools lsof zip unzip sudo sed
-        
+
         echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
         echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
         source ~/.bashrc
@@ -78,7 +80,7 @@ install_cuda_drivers(){
 
     echo "[error] OS type is unsupported. Exiting..."
     exit 0
-    
+
 }
 
 test_cuda(){
@@ -86,9 +88,10 @@ test_cuda(){
     nvcc --version
 }
 
-detect_os()
-detect_gcc()
-install_required_tools()
-install_cuda_drivers()
-test_cuda()
+
+detect_os
+detect_gcc
+install_required_tools
+install_cuda_drivers
+test_cuda
 
